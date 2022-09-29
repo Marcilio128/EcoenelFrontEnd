42b7eabcd9cfe515678135e92d8836de864a2fe1
Marcilio128 <marciliogomes2005@gmail.com>
2022-09-29 16:51:18 -0300

    responsividade

--- resources/views/DiaAtual.blade.php
+++ resources/views/DiaAtual.blade.php
@@ -5,6 +5,26 @@
 @section('content')
 
 @section('Dados')
- 
+<h1>Resumo Do Dia</h1>
+
+<h2>Beneficiados</h2>
+<p>92 Clientes</p>
+<div class="linhaHorizontal"></div>
+
+
+<h2>Bônus</h2>
+    <p>R$ 3.000</p>
+    <div class="linhaHorizontal"></div>
+
+<h2>Coletados</h2>
+    <p>16.888,98 kg</p>
+    <div class="linhaHorizontal"></div>
+
+<h2>Economizados</h2>
+    <p>70.865,33 KWH</p>
+
+
+
+
 @endsection('Dados')
 @endsection()
--- resources/views/Grafico.blade.php
+++ resources/views/Grafico.blade.php
@@ -16,23 +16,8 @@
         <div class="content">
             
             <div class="card">
-                <h1>Resumo Do Dia</h1>
-
-                <h2>Beneficiados</h2>
-                <p>92 Clientes</p>
-                <div class="linhaHorizontal"></div>
-                
-        
-                <h2>Bônus</h2>
-                    <p>R$ 3.000</p>
-                    <div class="linhaHorizontal"></div>
-        
-                <h2>Coletados</h2>
-                    <p>16.888,98 kg</p>
-                    <div class="linhaHorizontal"></div>
-        
-                <h2>Economizados</h2>
-                    <p>70.865,33 KWH</p>
+                @yield('Dados')
+              
             </div>                    
         </div>
     </div>
