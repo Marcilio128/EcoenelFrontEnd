78f0134b23016f52f32a25a69372dbb5f52cd09c
Marcilio128 <marciliogomes2005@gmail.com>
2022-10-21 15:09:52 -0300

    grafico

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -87,31 +87,6 @@
 
     <script>
         AOS.init();
-<<<<<<< HEAD
-/* const swiper = new Swiper('.swiper', {
-  // Optional parameters
-
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-}); */
-=======
-
->>>>>>> 99051648 (Ajustes nas cores do grafico e responsividade)
-
 
 
     </script>
